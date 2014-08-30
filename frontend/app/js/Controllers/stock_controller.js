8ed145520719417da2f04832a7236827afbce555
Zach Sherman <zksherm@gmail.com>
2014-08-29 22:23:25 -0400

    chartssss

--- frontend/app/js/Controllers/stock_controller.js
+++ frontend/app/js/Controllers/stock_controller.js
@@ -4,4 +4,40 @@ angular.module("app").controller('StockCtrl', function ($scope, $http, $routePar
     $scope.stock = data;
     console.log(data);
   });
+
+  var chart = $http.get('/api/stocks/chart/' + symbol).success(function(data) {
+    console.log(data);
+
+    var prices = [];
+
+    $.each(data.quotes.quote, function(i,v) {
+      var point = [];
+      point.push(parseFloat(moment.utc(v.datetime).unix() * 1000));
+      point.push(parseFloat(v.last));
+      prices.push(point); // might need to wrap in brackets
+    });
+
+    $scope.chart = prices;
+    console.log(prices);
+
+    $('.chart').highcharts('StockChart', {
+      rangeSelector : {
+          selected : 1,
+          inputEnabled: $('.chart').width() > 480
+      },
+
+      title : {
+          text : 'Stock Price'
+      },
+
+      series : [{
+          name : symbol,
+          data : $scope.chart,
+          tooltip: {
+            valueDecimals: 2
+          }
+      }]
+    });
+  });
+
 });
--- frontend/config/files.js
+++ frontend/config/files.js
@@ -12,6 +12,7 @@ module.exports = require(process.env["LINEMAN_MAIN"]).config.extend("files", {
       "vendor/bower/angular-resource/angular-resource.js",
       "vendor/bower/angular-route/angular-route.js",
       "vendor/bower/moment/moment.js",
+      "vendor/bower/highstock/js/highstock.src.js",
       "vendor/js/**/*.js"  // Note that this glob remains for traditional vendor libs
     ],
     app: [
